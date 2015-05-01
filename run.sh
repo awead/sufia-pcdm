d98327ed050b254ab17e1f4c6a22910dd2467951
Adam Wead <amsterdamos@gmail.com>
2015-05-01 14:38:01 -0400

    Minimal expression of a single-file deposited in Sufia

--- run.sh
+++ run.sh
@@ -10,26 +10,19 @@ curl -X DELETE ${BASE}/fcr:tombstone
 curl -X PUT ${BASE}
 
 
-# Generic File
-curl -i -X PUT -H "Content-Type: text/turtle" --data-binary @sufia-generic-file.ttl ${BASE}/gf/
+# Generic File 
+# An basic container (gf) with a direct container (files) that holds the user's original submitted
+# file (content), as well as a thumbnail derivative and FITS characterzation xml file.
+curl -i -X PUT -H "Content-Type: text/turtle" --data-binary @sufia-generic-file.ttl ${BASE}/gf
 curl -i -X PUT -H "Content-Type: text/turtle" --data-binary @pcdm-file.ttl          ${BASE}/gf/files
-curl -i -X PUT -H "Content-Type: image/jpeg"  --data-binary @content.jpg            ${BASE}/gf/content
-curl -i -X PUT -H "Content-Type: image/png"   --data-binary @thumbnail.png          ${BASE}/gf/thumbnail
-curl -i -X PUT -H "Content-Type: text/xml"    --data-binary @fits.xml               ${BASE}/gf/characterization
-# Create cover, page0 and page1
+curl -i -X PUT -H "Content-Type: image/jpeg"  --data-binary @content.jpg            ${BASE}/gf/files/content
+curl -i -X PUT -H "Content-Type: image/png"   --data-binary @thumbnail.png          ${BASE}/gf/files/thumbnail
+curl -i -X PUT -H "Content-Type: text/xml"    --data-binary @fits.xml               ${BASE}/gf/files/characterization
 
 
-# GenericFile
-
-# content file
-
-# thumbnail
-
-# characterization
-
-# The Work - ldp indirect container
-curl -i -X PUT -H "Content-Type: text/turtle" --data-binary @sufia-generic-work.ttl ${BASE}/work/
-curl -i -X PUT -H "Content-Type: text/turtle" --data-binary @ldp-indirect.ttl       ${BASE}/work/members/
+# Generic Work - ldp indirect container
+curl -i -X PUT -H "Content-Type: text/turtle" --data-binary @sufia-generic-work.ttl ${BASE}/work
+curl -i -X PUT -H "Content-Type: text/turtle" --data-binary @ldp-indirect.ttl       ${BASE}/work/members
 curl -i -X PUT -H "Content-Type: text/turtle" --data-binary @gf-proxy.ttl           ${BASE}/work/members/gf_proxy
 
 
