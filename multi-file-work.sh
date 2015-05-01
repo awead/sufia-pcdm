1fa384842ba864a02469901426e54e5251bc9193
Adam Wead <amsterdamos@gmail.com>
2015-05-01 15:19:35 -0400

    Multi-file example

---
+++ multi-file-work.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+BASE="http://fedoraAdmin:fedoraAdmin@localhost:8983/fedora/rest/dev"
+
+# Reset
+# ActiveFedora assumes a base path of /rest/dev
+curl -X DELETE ${BASE}
+curl -X DELETE ${BASE}/fcr:tombstone
+curl -X PUT ${BASE}
+
+# Generic File submissions
+# Represents a user submitting two files, a jpeg and a pdf. Each is chacterized using FITS, and 
+# a thumbnail is created.
+curl -i -X PUT -H   "Content-Type: text/turtle"               --data-binary @sufia-generic-file.ttl ${BASE}/gf1
+curl -i -X PUT -H   "Content-Type: text/turtle"               --data-binary @pcdm-multifile-gf1.ttl ${BASE}/gf1/files
+curl -i -X PUT -H   "Content-Type: image/jpeg"                --data-binary @file1/content.jpg      ${BASE}/gf1/files/content
+curl -i -X PATCH -H "Content-Type: application/sparql-update" --data-binary @pcdm-file.ru           ${BASE}/gf1/files/content/fcr:metadata
+curl -i -X PUT -H   "Content-Type: image/png"                 --data-binary @file1/thumbnail.png    ${BASE}/gf1/files/thumbnail
+curl -i -X PATCH -H "Content-Type: application/sparql-update" --data-binary @pcdm-file.ru           ${BASE}/gf1/files/thumbnail/fcr:metadata
+curl -i -X PUT -H   "Content-Type: text/xml"                  --data-binary @file1/fits.xml         ${BASE}/gf1/files/characterization
+curl -i -X PATCH -H "Content-Type: application/sparql-update" --data-binary @pcdm-file.ru           ${BASE}/gf1/files/characterization/fcr:metadata
+
+curl -i -X PUT -H   "Content-Type: text/turtle"               --data-binary @sufia-generic-file.ttl ${BASE}/gf2
+curl -i -X PUT -H   "Content-Type: text/turtle"               --data-binary @pcdm-multifile-gf2.ttl ${BASE}/gf2/files
+curl -i -X PUT -H   "Content-Type: image/jpeg"                --data-binary @file2/content.jpg      ${BASE}/gf2/files/content
+curl -i -X PATCH -H "Content-Type: application/sparql-update" --data-binary @pcdm-file.ru           ${BASE}/gf2/files/content/fcr:metadata
+curl -i -X PUT -H   "Content-Type: image/png"                 --data-binary @file2/thumbnail.png    ${BASE}/gf2/files/thumbnail
+curl -i -X PATCH -H "Content-Type: application/sparql-update" --data-binary @pcdm-file.ru           ${BASE}/gf2/files/thumbnail/fcr:metadata
+curl -i -X PUT -H   "Content-Type: text/xml"                  --data-binary @file2/fits.xml         ${BASE}/gf2/files/characterization
+curl -i -X PATCH -H "Content-Type: application/sparql-update" --data-binary @pcdm-file.ru           ${BASE}/gf2/files/characterization/fcr:metadata
+
+# Generic Work
+# A basic container (work) with an indirect container (members) to group two generic files into a work.
+curl -i -X PUT -H "Content-Type: text/turtle" --data-binary @sufia-generic-work.ttl ${BASE}/work
+curl -i -X PUT -H "Content-Type: text/turtle" --data-binary @ldp-indirect.ttl       ${BASE}/work/members
+curl -i -X PUT -H "Content-Type: text/turtle" --data-binary @gf1-proxy.ttl          ${BASE}/work/members/gf1_proxy
+curl -i -X PUT -H "Content-Type: text/turtle" --data-binary @gf2-proxy.ttl          ${BASE}/work/members/gf2_proxy
--- single-file-work.sh
+++ single-file-work.sh
@@ -2,7 +2,6 @@
 
 BASE="http://fedoraAdmin:fedoraAdmin@localhost:8983/fedora/rest/dev"
 
-
 # Reset
 # ActiveFedora assumes a base path of /rest/dev
 curl -X DELETE ${BASE}
@@ -15,17 +14,15 @@ curl -X PUT ${BASE}
 # file (content), as well as a thumbnail derivative and FITS characterzation xml file.
 curl -i -X PUT -H   "Content-Type: text/turtle"               --data-binary @sufia-generic-file.ttl ${BASE}/gf
 curl -i -X PUT -H   "Content-Type: text/turtle"               --data-binary @pcdm-file.ttl          ${BASE}/gf/files
-curl -i -X PUT -H   "Content-Type: image/jpeg"                --data-binary @content.jpg            ${BASE}/gf/files/content
+curl -i -X PUT -H   "Content-Type: image/jpeg"                --data-binary @file1/content.jpg      ${BASE}/gf/files/content
 curl -i -X PATCH -H "Content-Type: application/sparql-update" --data-binary @pcdm-file.ru           ${BASE}/gf/files/content/fcr:metadata
-curl -i -X PUT -H   "Content-Type: image/png"                 --data-binary @thumbnail.png          ${BASE}/gf/files/thumbnail
+curl -i -X PUT -H   "Content-Type: image/png"                 --data-binary @file1/thumbnail.png    ${BASE}/gf/files/thumbnail
 curl -i -X PATCH -H "Content-Type: application/sparql-update" --data-binary @pcdm-file.ru           ${BASE}/gf/files/thumbnail/fcr:metadata
-curl -i -X PUT -H   "Content-Type: text/xml"                  --data-binary @fits.xml               ${BASE}/gf/files/characterization
+curl -i -X PUT -H   "Content-Type: text/xml"                  --data-binary @file1/fits.xml         ${BASE}/gf/files/characterization
 curl -i -X PATCH -H "Content-Type: application/sparql-update" --data-binary @pcdm-file.ru           ${BASE}/gf/files/characterization/fcr:metadata
 
-
-# Generic Work - ldp indirect container
+# Generic Work
+# A basic container (work) with an indirect container (members) to group the generic file (gf) into a work.
 curl -i -X PUT -H "Content-Type: text/turtle" --data-binary @sufia-generic-work.ttl ${BASE}/work
 curl -i -X PUT -H "Content-Type: text/turtle" --data-binary @ldp-indirect.ttl       ${BASE}/work/members
 curl -i -X PUT -H "Content-Type: text/turtle" --data-binary @gf-proxy.ttl           ${BASE}/work/members/gf_proxy
-
-
